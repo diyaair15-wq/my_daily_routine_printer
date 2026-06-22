3f22add14b072868abf95229db5044546d25a0c4
Diya <diyaair15@gmail.com>
2026-06-22 19:23:28 +0700

    project 2

---
+++ storing_birthday.py
@@ -0,0 +1,16 @@
+# A program to store brithday dates inside five different variable
+
+
+birthday_1="2015-05-22"
+birthday_2="2017-09-15"
+birthday_3="2016-04-27"
+birthday_4="2013-11-26"
+birthday_5="2018-12-02"
+
+print("A program to store brithday dates inside five different variable\n")
+
+print(birthday_1)
+print(birthday_2)
+print(birthday_3)
+print(birthday_4)
+print(birthday_5)
