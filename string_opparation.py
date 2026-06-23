447ac58a8177385c8fa4e20011c722ffde20a3aa
Diya <diyaair15@gmail.com>
2026-06-23 11:21:32 +0700

    string_opparator.py

---
+++ string_opparation.py
@@ -0,0 +1,20 @@
+name="starlight"
+age=3
+is_student=True
+weight=20.5
+print("Name:", name)
+print("type of name:", type(name))
+print("Age:", age)
+print("type of age:", type(age))
+print("student:", is_student)
+print("type of student:", type(is_student))
+print("Weight:", weight)
+print("type of weight:", type(weight))
+# after typpecasting
+print("After typecasting:")
+age = str(age)
+print(age)
+print("data type of age:", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight:", type(weight))
