386f98ebf49606582946bc922690a9a8e4b787b8
Diya <diyaair15@gmail.com>
2026-06-24 10:58:19 +0700

    average.py

---
+++ average.py
@@ -0,0 +1,9 @@
+english=int(input("Enter the value: "))
+math=int(input("Enter the value: "))
+science=int(input("Enter the value: "))
+art=int(input("Enter the value: "))
+music=int(input("Enter the value: "))
+sum=english+math+science+art+music
+print("the sum of all the subjects is:",sum)
+average=sum/5
+print("the average of all the subjects is:",average)
