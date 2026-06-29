c6311d555e21336bae63d5eaf3e302233bc16ed1
Diya <diyaair15@gmail.com>
2026-06-29 11:12:15 +0700

    checking_number.py

---
+++ cheaking_number.py
@@ -0,0 +1,8 @@
+num=(int(input("Enter a number: ")))
+if num>11:
+    print("The number is greater than 11")
+    print("I am in if block")
+else:
+    print("The number is not greater than 11")
+    print("I am in else block")
+   
