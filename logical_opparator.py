50aa894389e682baab62b3010f03322584d4bbf2
Diya <diyaair15@gmail.com>
2026-07-01 11:13:20 +0700

    logical_opparator.py

---
+++ logical_opparator.py
@@ -0,0 +1,12 @@
+a="d"
+b="m"
+c="l"
+if not (a==b)and not (b==c):
+    print("both values are unequivalent")
+else:
+    print("both values are equivalent")
+A=10
+B=11
+if not ((A==B)or (B==11)): 
+    print("both values are unequivalent")
+    
