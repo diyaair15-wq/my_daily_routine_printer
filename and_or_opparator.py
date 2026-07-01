35422a05133698c709a6369c9d7df3f51cf4a716
Diya <diyaair15@gmail.com>
2026-07-01 10:54:50 +0700

    and_or_opparator.py

---
+++ and_or_opparator.py
@@ -0,0 +1,14 @@
+a=10
+b=11
+c=21
+if a < c and b < c:
+    print("c is greater than a and b")
+else:
+    print("atleast one of the values are not  greater than c")
+a=10
+b=11
+c=21
+if a or b < c:
+    print("Atleast one of the values is greater than c")
+else:
+    print("none of the values are  greater than c")    
