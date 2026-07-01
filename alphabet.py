8aa4afa9ba9be26a1f27749afa66be5c928835f8
Diya <diyaair15@gmail.com>
2026-07-01 17:56:20 +0700

    alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,12 @@
+x=input('please enter a character> ')
+if x.isalpha():
+    print('the character is an alphabet')
+else:
+    print('the character is not an alphabet')
+    
+    
+if x<='z' and x>='a' or x<='Z' and x>='A':
+    print('the character is a lowercase alphabet')
+else:
+    print('the character is not an alphabet')
+    
