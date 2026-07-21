8b6abe9fcc7b66d284ff9f1148aa0cc54d358147
Diya <diyaair15@gmail.com>
2026-07-21 11:37:36 +0700

    square.py

---
+++ square.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.Screen().bgcolor("red")
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+
+turtle.done()
