c4e1b8ae57373b310e3daeba26eec57055677c04
Diya <diyaair15@gmail.com>
2026-07-22 11:14:20 +0700

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,32 @@
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+
+def subtract(a, b):
+    return (a - b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Subtraction of {a} and {b} is {subtract(a, b)}')
+
+def multiply(a, b):
+    return (a * b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Multiplication of {a} and {b} is {multiply(a, b)}')
+
+def divide(a, b):
+    return (a / b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Division of {a} and {b} is {divide(a, b)}')
