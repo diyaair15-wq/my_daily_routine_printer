1ed3be10757e690a03c51ac403adf5bbfd1a62ab
Diya <diyaair15@gmail.com>
2026-07-22 11:07:27 +0700

    weather condition.py

---
+++ weather conditio.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print("the weather condition is pleasant in:",spring)
+    print("the weather condition is the same in:",autumn)
+    
+spring = "autumn"
+autumn = "spring"
+weather_condition()
