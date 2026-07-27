7a68c627a32c53958fe13953e041f4822543ed8d
Diya <diyaair15@gmail.com>
2026-07-27 15:44:41 +0700

    shut down.py

---
+++ shutdown.py
@@ -0,0 +1,11 @@
+print("please use capital for first letterbut use lowercase letters for the rest")
+a= int(input("do you want to shut it down? "))
+if a == Yes: # type: ignore
+    print("shutting down")
+
+elif a == No: # type: ignore
+    print("shutting down in a minute")
+
+else:
+    print ("sorry i don't understand")
+    
