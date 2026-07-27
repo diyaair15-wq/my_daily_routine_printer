39d682d286d799148cfdde03c84be1757a2bdb16
Diya <diyaair15@gmail.com>
2026-07-27 16:49:42 +0700

    due amount.py

---
+++ due amount.py
@@ -0,0 +1,11 @@
+def subtract(a, b):
+    return (a - b)
+
+a = float(input('Enter 1st number: '))
+b = float(input('Enter 2nd number: '))
+
+print(f'Subtraction of {a} and {b} is {subtract(a, b)}')
+
+c= subtract(a, b)
+
+print("due amount is",(c))
