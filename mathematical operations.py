43297a3a09c6e315a997317e966869edb086cfb1
Diya <diyaair15@gmail.com>
2026-08-06 10:41:09 +0700

    mathematical operations .py

---
+++ mathematical operations.py
@@ -0,0 +1,12 @@
+import math
+print("the floor and ceiling f 23.56 are:",str(math.floor(23.56)) +","+ str(math.ceil(23.56)))
+
+x=10
+y=-15
+x=y
+print("the value of x after copying the sign of y is:",str(math.copysign(x,y)))
+
+print("absolute value of -96 and 56 are:" + str(math.fabs(-96)) + "," + str(math.fabs(56)))
+
+print("the gcd of 24 and 56 is:" + str(math.gcd(24,56)))
+
