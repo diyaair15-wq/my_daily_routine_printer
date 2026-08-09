a2a30b5c595be21fd6f1af21f6ef01cdb24469d5
Diya <diyaair15@gmail.com>
2026-08-09 13:16:42 +0700

    trigonometry.py

---
+++ trigonometry.py
@@ -0,0 +1,6 @@
+import math
+print("enter a number in degrees to find its trigonometric values:")
+n = int(input())
+print("cosine of", n, "is", math.cos(math.radians(n)))
+print("sine of", n, "is", math.sin(math.radians(n)))
+print("tangent of", n, "is", math.tan(math.radians(n)))
