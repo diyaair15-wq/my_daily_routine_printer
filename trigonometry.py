87d2bd553fd48ac94e9eb8d2dbefca08afc45aec
Diya <diyaair15@gmail.com>
2026-08-11 10:53:50 +0700

    tuple operations.py

--- trigonometry.py
+++ trigonometry.py
@@ -1,6 +1,6 @@
 import math
 print("enter a number in degrees to find its trigonometric values:")
 n = int(input())
-print("cosine of", n, "is", math.cos(math.radians(n)))
-print("sine of", n, "is", math.sin(math.radians(n)))
-print("tangent of", n, "is", math.tan(math.radians(n)))
+print("cosine of", n, "is", math.cos(n))
+print("sine of", n, "is", math.sin(n))
+print("tangent of", n, "is", math.tan(n))
