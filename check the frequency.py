b9f601e8899f6568942eeabfe5fcc1e538115067
Diya <diyaair15@gmail.com>
2026-08-13 11:13:07 +0700

    check the frequency.py

---
+++ check the frequency.py
@@ -0,0 +1,17 @@
+test_dict = {'codingal': 2, 'is': 2, 'best': 2, 'for': 2, 'coding': 1}
+
+
+
+print("The original dictionary is : " + str(test_dict))
+
+
+k = 2
+
+
+
+res = 0
+for key in test_dict:
+    if test_dict[key] == k:
+        res += 1
+
+print("frequency of k is : " + str(res))
