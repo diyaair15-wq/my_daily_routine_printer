b8274e72358b0660a4da38af25ce8ab409fc0a25
Diya <diyaair15@gmail.com>
2026-08-17 10:51:07 +0700

    operations on set.py

---
+++ set.py
@@ -0,0 +1,18 @@
+my_set = {1,2,3}
+print(my_set)
+
+my_set = {1.0,"Hello", (1,2,3)}
+print(my_set)
+
+my_set = {1,2,3,4,3,2}
+print(my_set)
+
+my_set = {1,2,3,2}
+print(my_set)
+
+num_set = set([0,1,3,4,5])
+print("original set:")
+print(num_set)
+num_set.pop()
+print("After removing an element from the set:")
+print(num_set)
