801d8b965784a895d1be34f8b0d9a3d26b92136d
Diya <diyaair15@gmail.com>
2026-08-26 18:31:19 +0700

    times on map.py

---
+++ hands on map.py
@@ -0,0 +1,12 @@
+numbers1 = [1,2,3]
+numbers2 = [4,5,6]
+result = map(lambda x, y: x + y, numbers1, numbers2)
+print(list(result))
+
+
+nums = [1, 2, 3, 4, 5]
+def sq(n):
+    return n * n
+squared = list(map(sq, nums))
+print("square of numbers in list:")
+print(squared)
